e15dd3a856892b4fe21447370047514334c7adb3
19013na <19013@naver.com>
2025-05-08 17:32:59 +0900

    feat : controller ì ã 및 exception 추가

--- src/main/java/com/rookies3/myspringbootlab/controller/BookController.java
+++ src/main/java/com/rookies3/myspringbootlab/controller/BookController.java
@@ -1,9 +1,67 @@
 package com.rookies3.myspringbootlab.controller;
 
+import com.rookies3.myspringbootlab.entity.Book;
+import com.rookies3.myspringbootlab.exception.BusinessException;
+import com.rookies3.myspringbootlab.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/books")
 public class BookController {
 
+    private final BookRepository bookRepository;
+
+    @PostMapping
+    public Book create(@RequestBody Book book){
+        return bookRepository.save(book);
+    }
+
+    @GetMapping
+    public List<Book> getAll(){
+        return bookRepository.findAll();
+    }
+
+    //ID로 특정 도서 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id){
+        Optional<Book> book = bookRepository.findById(id);
+        return book.map(ResponseEntity::ok)
+                .orElse(new ResponseEntity("Book Not Found", HttpStatus.NOT_FOUND));
+    }
+
+    //ISBN으로 도서 조회
+    @GetMapping("/isbn/{isbn}")
+    public Book getUserByIsbn(@PathVariable String isbn){
+        Optional<Book> book = bookRepository.findByIsbn(isbn);
+        return book.orElseThrow(() ->
+                new BusinessException("Book Not Found", HttpStatus.NOT_FOUND));
+    }
+
+    //도서 정보 수정
+    @PatchMapping("/{id}")
+    public ResponseEntity<Book> updateBookById(@PathVariable Long id, @RequestBody Book book){
+        Book findBook = bookRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("Book Not Found", HttpStatus.NOT_FOUND));
+        findBook.setPrice(book.getPrice());
+        findBook.setAuthor(book.getAuthor());
+        return ResponseEntity.ok(bookRepository.save(findBook));
+    }
+
+    //도서 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBookById(@PathVariable Long id){
+        Book findBook = bookRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("Book Not Found", HttpStatus.NOT_FOUND));
+        bookRepository.delete(findBook);
+        return ResponseEntity.ok("Book deleted");
+    }
 }
---
+++ src/main/java/com/rookies3/myspringbootlab/exception/advice/ErrorObject.java
@@ -0,0 +1,24 @@
+package com.rookies3.myspringbootlab.exception.advice;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+// 에러정보를저장하는DTO 클래스
+// 해당 데이터는 lombok
+
+@Data
+public class ErrorObject {
+    private Integer statusCode;
+    private String message;
+    private String timestamp;
+
+    public String getTimestamp() {
+        LocalDateTime ldt = LocalDateTime.now();
+        return DateTimeFormatter.ofPattern(
+                "yyyy-MM-dd HH:mm:ss E a", 
+                Locale.KOREA).format(ldt);
+    }
+}
